8c0b56a46bd8a37937b5317fb7acbf72e4a165a3
jrmygp <fat.snorlax17@gmail.com>
2022-03-02 18:41:18 +0700

    created day4

---
+++ Day4/index.jsx
@@ -0,0 +1,8 @@
+const [myUsername, setMyUsername] = useState("seto");
+const changeUsername = () => {
+    setMyUsername("bill");
+  };
+
+
+  // <h1>{myUsername}</h1>
+  // <Button onClick={changeUsername}>Change My Username</Button>
